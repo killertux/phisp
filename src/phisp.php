aef30cb8a54e15ee3d970fb843ea5b23bca325c3
Bruno Clemente <bruno.canguru@hotmail.com>
2022-02-03 22:07:23 -0300

    Step 0 mal

---
+++ src/phisp.php
@@ -0,0 +1,30 @@
+#!/usr/bin/php
+<?php
+
+$stdin = fopen("php://stdin", "r");
+print_cursor();
+
+while (($data = fgets($stdin)) !== false) {
+    echo rep($data);
+    print_cursor();
+}
+
+function read(string $command): string {
+    return $command;
+}
+
+function evaluation(string $command): string {
+    return $command;
+}
+
+function print_code(string $command): string {
+    return $command;
+}
+
+function rep(string $command) : string {
+    return print_code(evaluation(read($command)));
+}
+
+function print_cursor(): void {
+    echo "user> ";
+}
